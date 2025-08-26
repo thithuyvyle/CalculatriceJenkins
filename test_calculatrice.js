12389aea1d0f92a3fac5f6dee2787301d2cd1328
thithuyvyle <thithuyvyle@hotmail.com>
2025-08-26 15:02:28 +0200

    inital commit

---
+++ test_calculatrice.js
@@ -0,0 +1,30 @@
+const { Builder, By, Key } = require('selenium-webdriver');
+
+(async function testCalculatrice() {
+    // Initialiser le driver avec les bonnes options
+
+    try {
+        // Accéder au site
+
+        // --- Test 1 : Vérifier l'Addition ---
+
+        // Afficher les résultats
+
+        // --- Test 2 : Division par Zéro ---
+
+        // Afficher les résultats
+
+        // --- Test 3 : Entrée Non Valide ---
+
+        // Afficher les résultats
+
+        // --- Test 4 : Vérifier la Soustraction ---
+        
+        // Afficher les résultats
+
+
+    } finally {
+        // Fermer le navigateur
+    }
+})();
+
